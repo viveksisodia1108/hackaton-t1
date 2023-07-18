c7ce172f632294df10beca933122031cdcd7f0b5
Swati Awasthi <swati.awasthi@opsview.com>
2023-07-18 14:11:11 +0100

    yml commits

--- database/database.go
+++ database/database.go
@@ -19,7 +19,7 @@ var DB Dbinstance
 
 func ConnectDb() {
 	dsn := fmt.Sprintf(
-		"host=pgdb user=%s password=%s dbname=%s port=5432 sslmode=disable TimeZone=UTC",
+		"host=db user=%s password=%s dbname=%s port=5432 sslmode=disable TimeZone=UTC",
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
 		os.Getenv("DB_NAME"),
