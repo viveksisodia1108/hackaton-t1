1bea10a5b5e476994e3e005c440602a9373a2426
Swati Awasthi <swati.awasthi@opsview.com>
2023-07-18 15:34:52 +0100

    more changes as well

--- database/database.go
+++ database/database.go
@@ -19,7 +19,8 @@ var DB Dbinstance
 
 func ConnectDb() {
 	dsn := fmt.Sprintf(
-		"host=db user=%s password=%s dbname=%s port=5432 sslmode=disable TimeZone=UTC",
+		"host=%s user=%s password=%s dbname=%s port=5432 sslmode=disable TimeZone=UTC",
+		os.Getenv("DB_HOST"),
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
 		os.Getenv("DB_NAME"),
